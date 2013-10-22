97e859e4e15e39df57d3cd4f7f70c45ba2d5e4f1
Cedric Porter <cedricporter@gmail.com>
2013-10-22 14:16:02 +0800

    install emacs

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# File: install.sh
+#
+# Created: 星期二, 十月 22 2013 by Hua Liang[Stupid ET] <et@everet.org>
+#
+
+mkdir software
+cd software
+
+# Emacs
+if [ ! -f "emacs-24.3.tar.gz" ]; then
+    wget http://ftpmirror.gnu.org/emacs/emacs-24.3.tar.gz
+fi
+tar zxvf emacs-24.3.tar.gz
+(cd emacs-24.3 && ./configure --prefix=$HOME/local && make -j4)
